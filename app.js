61e1db2c7b05f29d800e78856a12c956f793583e
umair <muhammadumair9290@gmail.com>
2017-05-05 15:37:56 +0500

    first commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-
 function addTask() {
     var userInput = document.getElementById("task"),
         list = document.createElement("LI"),
-        ran = (Math.random()*100000000).toString(),
-        id = ran.substring(0, 8);
+        ranNum = (Math.random()*100000000).toString(),
+        id = ranNum.substring(0, 8);
 
     list.innerHTML = userInput.value;
     userInput.value="";
     var tasks = document.getElementById("list");
         tasks.appendChild(list);
-    var att = tasks.setAttribute("id", id),
-        btn = document.createElement("BUTTON");
+    list.setAttribute("id", id);
+    var btn = document.createElement("BUTTON");
     var t = document.createTextNode("X");
     //var idVal = att.getAttribute("id");
         btn.appendChild(t);
-    var btnX = list.appendChild(btn);
-        btn.setAttribute('onclick','del()');
-    var idVal = list.getAttribute(id);
-    console.log(idVal);
+    list.appendChild(btn);
+        btn.setAttribute('onclick','del(event)');
+    list.getAttribute(id);
+    //console.log(idVal);
 
 
 }
 
 
 function del() {
-    document.getElementById()
-
-
+    var target = event.target,
+        parentTarget = target.parentElement;
+        //childTarget = parentTarget.child;
+        parentTarget.remove();
 }
