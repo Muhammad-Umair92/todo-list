a3d2c5846c3c7412176543d7bb74dbdadc566005
umair <muhammadumair9290@gmail.com>
2017-05-06 20:06:55 +0500

    first commit

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
+var userInput = document.getElementById("task");
+//var taskId = document.getElementById("list").getElementsByTagName("LI");
+//console.log(taskId.value);
+function condition(){
+    if(document.getElementById("btnAddTask").innerText=="Add Task"){
+        addTask();
+    }
+    else{
+        update();
+    }
+}
 function addTask() {
-    var userInput = document.getElementById("task"),
-        list = document.createElement("LI"),
-        ranNum = (Math.random()*100000000).toString(),
-        id = ranNum.substring(0, 8);
 
-    list.innerHTML = userInput.value;
+     var para = document.createElement("P"),
+         list = document.createElement("LI"),
+         ranNum = (Math.random()*100000000).toString(),
+         id = ranNum.substring(0, 8);
+
+    //list.innerHTML = userInput.value;
+    para.innerHTML = userInput.value;
     userInput.value="";
     var tasks = document.getElementById("list");
-        tasks.appendChild(list);
+    tasks.appendChild(list);
+    list.appendChild(para);
     list.setAttribute("id", id);
-    var btn = document.createElement("BUTTON");
-    var t = document.createTextNode("X");
-    //var idVal = att.getAttribute("id");
-        btn.appendChild(t);
-    list.appendChild(btn);
-        btn.setAttribute('onclick','del(event)');
-    list.getAttribute(id);
-    //console.log(idVal);
+    var btnX = document.createElement("BUTTON"),
+        btnXVal = document.createTextNode("X"),
+        btnEdit = document.createElement("BUTTON"),
+        btnEditVal = document.createTextNode("Edit");
+
+    btnX.appendChild(btnXVal);
+    list.appendChild(btnX);
+    btnX.setAttribute('onclick','del(event)');
+
+    btnEdit.appendChild(btnEditVal);
+    list.appendChild(btnEdit);
+    btnEdit.setAttribute('onclick','edit(event)');
+
 
 
 }
@@ -25,6 +44,51 @@ function addTask() {
 function del() {
     var target = event.target,
         parentTarget = target.parentElement;
-        //childTarget = parentTarget.child;
         parentTarget.remove();
+}
+
+
+function edit() {
+    //console.log(event)
+    /*steps
+     * 1) Store id of LI in a variable, lets say var taskId = '234234df'
+     * 2) Get element of AddTaskBtn in a variable, var addTaskBtn = <elementcode>
+     * 3) change the value to addTaskBtn to 'update'
+     * 4) Also set taskId to addTaskBtn as an attribute.
+     * */
+    var target = event.target,
+        parentTarget = target.parentElement,
+        listTargetId = parentTarget.id,
+        addTaskButton = document.getElementById("btnAddTask");
+    addTaskButton.innerText = "Update";
+    addTaskButton.setAttribute("task_id", listTargetId);
+    //console.log(listTargetId);
+    //var taskId = document.getElementById("list");
+    //console.log(taskId);
+    //var listId = taskId.getElementsByTagName("LI");
+    //console.log(listId[0].id);
+    var editTarget = event.target,
+        parentTarget = editTarget.parentElement,
+        pTxt = (parentTarget.getElementsByTagName("P")),
+        InputBtn = document.getElementById("btnAddTask").innerText = "Update",
+        valPTxt = (pTxt[0].innerText);
+    //console.log(InputBtn);
+    //console.log(userInput.innerText);
+    userInput.value = valPTxt;
+
+}
+function update(){
+
+        var updateTarget = event.target,
+            id_list = updateTarget.getAttribute("task_id"),
+            list_txt = document.getElementById(id_list);
+            //console.log(userInput);
+            list_txt.children[0].innerText = userInput.value;
+            document.getElementById("btnAddTask").innerText = "Add Task";
+            userInput.value="";
+
+
+
+        //document.getElementById()
+        //console.log(document.getElementById("id").getElementsByTagName("p"));
 }
